534b28b1606c46848ebed5305f8cb8a9eeb5e0f6
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-05-12 15:34:47 +0200

    feat: create rotte e layout

---
+++ components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header (){
+    return(
+        <header>
+            <h1>logooo</h1>
+        </header>
+    )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-
+// importazione file
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Movies from "../pages/movies"
+import DefaultLayout from "../layout/defaultLayout"
 function App() {
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route path="/" element={<div>home</div>} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/movies/:id" element={<div>movies reviews</div>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
