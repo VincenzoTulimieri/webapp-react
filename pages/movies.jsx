31d2793f95e5134deb22b5d6069c904abc3b49b5
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-05-12 20:34:03 +0200

    feat: creato form di ricerca

--- pages/movies.jsx
+++ pages/movies.jsx
@@ -2,26 +2,47 @@ import { useEffect, useState } from "react";
 import MovieCard from "../components/MovieCard";
 import axios from "axios";
 
-export default function Movies (){
+export default function Movies() {
 
-    const [movies, setMovies]= useState([])
+    const [movies, setMovies] = useState([])
+    const [search, setSearch] = useState('')
 
-    function apiMovies(){
-        axios.get('http://127.0.0.1:3000/movies')
-            .then(response =>{
+    function apiMovies() {
+        axios.get('http://127.0.0.1:3000/movies',{
+            params:{
+                search
+            }
+        })
+            .then(response => {
                 setMovies(response.data)
             })
+            .catch(err=>consol.log(err))
     }
 
-    useEffect(apiMovies,[])
+    useEffect(apiMovies, [])
 
-    return(
+    function searchMovies(event){
+        event.preventDefault()
+        apiMovies()
+    }
+
+    return (
         <div className="container my-3">
             <h1 className="mb-3 vt-text">Film</h1>
             <section>
-                <h2 className="mb-3 vt-text">I migliori Film da vedere</h2>
+                <div className="vt-search">
+                    <h2 className="mb-3 vt-text">I migliori Film da vedere</h2>
+                    <form className="row g-3" onSubmit={searchMovies}>
+                        <div className="col-auto">
+                            <input type="text" className="form-control" placeholder="Cerca" value={search} onChange={(event)=>setSearch(event.target.value)} />
+                        </div>
+                        <div className="col-auto">
+                            <button type="submit" className="btn btn-primary mb-3">Cerca</button>
+                        </div>
+                    </form>
+                </div>
                 <div className="row">
-                    <MovieCard movies={movies}/>
+                    <MovieCard movies={movies} />
                 </div>
             </section>
         </div>
