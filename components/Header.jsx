ed11919ab033b3a330414270e95bacad09642b85
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-05-12 17:58:48 +0200

    feat: creato header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,14 @@
+import { NavLink } from "react-router-dom";
+
 export default function Header (){
     return(
-        <header>
-            <h1>logooo</h1>
+        <header className="bg-danger py-2 mb-3">
+            <div className="container d-flex justify-content-between">
+                <div className="container-logo">logooo</div>
+                <ul>
+                    <NavLink to={'/movies'}>Film</NavLink>
+                </ul>
+            </div>
         </header>
     )
 }
