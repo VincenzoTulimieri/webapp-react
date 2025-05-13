7110495c76d6cef17b631d27972a4a67abd1c26a
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-05-13 22:13:27 +0200

    feat: crato render per le recensioni

--- pages/MovieDetails.jsx
+++ pages/MovieDetails.jsx
@@ -1,15 +1,44 @@
-import { useParams } from "react-router-dom"
+// importazione file
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import ReviewsCard from "../components/ReviewsCard";
 
 export default function MovieDetails(){
 
     const {id} = useParams()
-    console.log(id)
+    const [movie, setMovie] = useState([])
+
+    function getMovie() {
+        axios.get(`http://127.0.0.1:3000/movies/${id}`)
+            .then(response => {
+                setMovie(response.data)
+            })
+            .catch(err=>console.log(err))
+    }
+
+    useEffect(getMovie, [id])
 
     return(
-        <div className="container">
-            <h1 className="vt-text-color">film nel dettaglio</h1>
-        </div>
-        
+        <article className="container">
+            <header className="d-flex gap-4 mb-3">
+                <div className="vt-container-img">
+                    <img src={movie?.imgPath} alt={movie?.title} />
+                </div>
+                <div>
+                    <h1 className="vt-text-color vt-text-dimension">{movie?.title}</h1>
+                    <p className="vt-text-color">Regia: <strong>{movie?.director}</strong></p>
+                    <p className="vt-text-color">{movie?.abstract}</p>
+                </div>
+            </header>
+            <section>
+                <div className="mb-2 vt-flex">
+                    <h4 className="vt-text-color">Le migliori Recensioni</h4>
+                    <p className="vt-text-color">{movie.reviews_vote}</p>
+                </div>
+                {movie.reviews?.map(review =><ReviewsCard key={review.id} data={review} />)}
+            </section>
+        </article>
         
     )
 }
