58c6d70c78bf34eaff64f614fd7045bf15850dd9
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-05-13 22:41:32 +0200

    feat: inserita logica

--- components/StarRating.jsx
+++ components/StarRating.jsx
@@ -1,4 +1,7 @@
-export default function StarRating(){
+export default function StarRating({ vote }) {
     const maxValue = 5;
-    
+
+    return (
+        [...Array(maxValue)].map((value, index) => index < vote ? <i key={index} className="fa-solid fa-star text-warning"></i> : <i key={index} className="fa-regular fa-star text-warning"></i>)
+    )
 }
