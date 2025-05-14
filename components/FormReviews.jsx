601067f7c4b74226c712b6b1d72e4a4184094cd9
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-05-15 00:47:47 +0200

    feat: form completo

--- components/FormReviews.jsx
+++ components/FormReviews.jsx
@@ -1,7 +1,57 @@
-export default function FormReviews({id,getMovie}){
-    return(
-        <div>
-            qui il form 
-        </div>
+import { useState } from "react"
+
+export default function FormReviews({ id, getMovie }) {
+    const initialFormData={
+        name:'',
+        vote:1,
+        text:''
+    }
+    const [formData, setFromData]= useState(initialFormData)
+
+
+
+    function sendData(event){
+        event.preventDefault()
+        console.log(event.target)
+
+        const{name,value}= event.target
+
+        let correctValue =value
+        if(name === 'vote'){
+            correctValue = parseInt(value)
+        }
+
+        setFromData((formData)=>({
+            ...formData,
+            [name]:correctValue
+        }))
+        console.log(formData)
+    }
+
+    return (
+        <form action="">
+            <div className="card">
+                <div className="card-header">
+                    <h4>Scrivi Qui una tua Recensione</h4>
+                </div>
+                <div className="card-body">
+                    <div className="mb-3">
+                        <label htmlFor="reviews-name" className="form-label">Nome</label>
+                        <input type="text" className="form-control" id="reviews-name" placeholder="Inserisci in tuo nome" value={formData.name} name="name" onChange={sendData}/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="form-vote" className="form-label">Voto</label>
+                        <input type="number" min={1} max={5} className="form-control" id="form-vote" placeholder="Inserici un voto" value={formData.vote} name="vote"onChange={sendData} />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="form-text" className="form-label">Testo Recensione</label>
+                        <textarea className="form-control" id="form-text" rows="3" value={formData.text} name="text" onChange={sendData}></textarea>
+                    </div>
+                    <div className="mb-3">
+                       <button type="submit" className="btn btn-danger mb-3">Aggiungi</button>
+                    </div>
+                </div>
+            </div>
+        </form>
     )
 }
