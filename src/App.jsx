a2c41807a1a9479441a88544c41c720444fb8d38
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-05-16 21:16:05 +0200

    feat: importato contesto e iniziato loader

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,26 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import DefaultLayout from "./layout/defaultLayout"
 import Movies from "./pages/Movies"
 import MovieDetails from "./pages/MovieDetails"
+import GlobalContext from "./contexts/GlobalContext"
+import { useState } from "react"
 
 function App() {
+
+  const[loader, setLoader]= useState(false)
+
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<DefaultLayout />}>
-            <Route path="/movies" element={<Movies />} />
-            <Route path="/movies/:id" element={<MovieDetails />} />
-          </Route>
-          <Route path="*" element={<div><h1>Pagina non trovata</h1></div>}/>
-        </Routes>
-      </BrowserRouter>
+      <GlobalContext.Provider value={{loader,setLoader}}>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<DefaultLayout />}>
+              <Route path="/movies" element={<Movies />} />
+              <Route path="/movies/:id" element={<MovieDetails />} />
+            </Route>
+            <Route path="*" element={<div><h1>Pagina non trovata</h1></div>} />
+          </Routes>
+        </BrowserRouter>
+      </GlobalContext.Provider>
     </>
   )
 }
