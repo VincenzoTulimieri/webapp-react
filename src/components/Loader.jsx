fbdce9b5a984c9cca5eca7ca3b13d02c837c939a
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-05-18 14:02:11 +0200

    fix: corretta classe container

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,6 +1,6 @@
 export default function Loader(){
     return(
-        <div className="contaier">
+        <div className="container">
             <h1 className="vt-text-color">Caricamento in corso...</h1>
         </div>
     )
