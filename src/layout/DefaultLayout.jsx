0fa6ebe736777fc1d96fb4f99c325d7020d28b8e
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-05-18 12:07:49 +0200

    feat: inserito componente loader

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,13 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
+import { useContext } from "react";
+import GlobalContext from "../contexts/GlobalContext";
+import Loader from "../components/Loader";
 
 export default function DefaultLayout (){
+    const {isLoader}= useContext(GlobalContext)
     return(
         <>
         <Header />
         <main>
             <Outlet />
         </main>
+        {isLoader && <Loader />}
         </>
     )
 }
