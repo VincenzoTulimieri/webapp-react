7c3443d663c42fb60d488db1a625b305c1a4c839
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-05-21 21:38:28 +0200

    feat: creata pagina add movies

---
+++ src/pages/AddNewMovie.jsx
@@ -0,0 +1,9 @@
+export default function AddNewMovies(){
+    return(
+        <>
+        <div className="container">
+            <h1>aggiungi il tuo film</h1>
+        </div>
+        </>
+    )
+}
