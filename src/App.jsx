20dae821ddc996bcf5493bfcca67b0b83bcf2fe9
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-05-26 20:51:18 +0200

    feat: pagina di benvenuto

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,19 @@ import MovieDetails from "./pages/MovieDetails"
 import GlobalContext from "./contexts/GlobalContext"
 import { useState } from "react"
 import AddNewMovies from "./pages/AddNewMovie"
+import WelcomePage from "./pages/WelcomePage"
 
 function App() {
 
-  const[isLoader, setIsLoader]= useState(false)
+  const [isLoader, setIsLoader] = useState(false)
 
   return (
     <>
-      <GlobalContext.Provider value={{isLoader,setIsLoader}}>
+      <GlobalContext.Provider value={{ isLoader, setIsLoader }}>
         <BrowserRouter>
           <Routes>
             <Route element={<DefaultLayout />}>
+              <Route path="/" element={<WelcomePage />} />
               <Route path="/movies" element={<Movies />} />
               <Route path="/add-movie" element={<AddNewMovies />} />
               <Route path="/movies/:id" element={<MovieDetails />} />
---
+++ src/pages/WelcomePage.jsx
@@ -0,0 +1,7 @@
+export default function WelcomePage(){
+    return(
+        <>
+        <h1>WELCOME PAGE</h1>
+        </>
+    )
+}
