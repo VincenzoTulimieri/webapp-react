12c0f29aa41d842a246be6ff349379426175d607
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-05-26 20:51:49 +0200

    fix: path del link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
         <header className="bg-danger py-2 mb-4">
             <div className="container d-flex align-items-center gap-4">
                 <div className="vt-container-logo">
-                    <Link to={'/movies'}><img src="../logo-grande.png" alt="" className="w-100" /></Link>
+                    <Link to={'/'}><img src="../logo-grande.png" alt="" className="w-100" /></Link>
                 </div>
                 <h1 className="vt-text-color vt-text-dimension">Movie time</h1>
             </div>
