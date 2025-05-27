f12e2a6d52e45744b7aa4d8c08063a95a883a2b8
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-05-27 19:42:19 +0200

    fix: cambiato titolo alla pagina

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function WelcomePage() {
     return (
         <div className="container vt-container-welcome vt-text-dimension">
-            <h1 className="vt-text-color">I migliori film da vedere</h1>
+            <h1 className="vt-text-color">Ti consigliamo i migliori film da vedere</h1>
             <Link to={'/movies'} className="btn btn-danger">Vai ai film</Link>
         </div>
     )
