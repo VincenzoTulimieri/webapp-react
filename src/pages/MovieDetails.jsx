b93291e463e884ea6e4ec18d600d22a831855a30
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2026-03-26 14:15:37 +0100

    fix: stile pagina dettagli film

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -8,12 +8,12 @@ import FormReviews from "../components/FormReviews";
 import { useContext } from "react";
 import GlobalContext from "../contexts/GlobalContext";
 
-export default function MovieDetails(){
+export default function MovieDetails() {
 
-    const {id} = useParams()
+    const { id } = useParams()
     const navigate = useNavigate()
     const [movie, setMovie] = useState([])
-    const {setIsLoader}= useContext(GlobalContext)
+    const { setIsLoader } = useContext(GlobalContext)
 
     function getMovie() {
         setIsLoader(true)
@@ -21,36 +21,55 @@ export default function MovieDetails(){
             .then(response => {
                 setMovie(response.data)
             })
-            .catch(err=>console.log(err))
-            .finally(()=>setIsLoader(false))
+            .catch(err => console.log(err))
+            .finally(() => setIsLoader(false))
     }
 
     useEffect(getMovie, [id])
 
-    return(
+    return (
+
         <article className="container">
-            <header className="d-flex gap-4 mb-3">
-                <div className="vt-container-img">
+
+            <div className="vt-detail-card">
+
+                <div className="vt-detail">
+
                     <img src={movie?.imgPath} alt={movie?.title} />
+
+                    <div className="vt-detail-info">
+                        <h1>{movie?.title}</h1>
+
+                        <div className="vt-meta">
+                            <span>{movie?.director}</span>
+                            <span>{movie?.genre}</span>
+                        </div>
+
+                        <div className="vt-rating">
+                            <StarRating vote={movie?.reviews_vote} />
+                        </div>
+
+                        <p className="vt-description">
+                            {movie?.abstract}
+                        </p>
+
+                    </div>
+
                 </div>
-                <div>
-                    <h1 className="vt-text-color vt-text-dimension">{movie?.title}</h1>
-                    <p className="vt-text-color">Regia: <strong>{movie?.director}</strong></p>
-                    <p className="vt-text-color">{movie?.abstract}</p>
-                </div>
-            </header>
+
+            </div>
             <section>
                 <div className="mb-2 vt-flex">
                     <h4 className="vt-text-color">Le migliori Recensioni</h4>
-                    <p className="vt-text-color"><StarRating vote={movie?.reviews_vote}/></p>
+                    <p className="vt-text-color"><StarRating vote={movie?.reviews_vote} /></p>
                 </div>
-                {movie.reviews?.length ? movie.reviews.map(review =><ReviewsCard key={review.id} data={review} />) : <div><h1 className="vt-text-color">Nessuna recensione trovata</h1></div>}
+                {movie.reviews?.length ? movie.reviews.map(review => <ReviewsCard key={review.id} data={review} />) : <div><h1 className="vt-text-color">Nessuna recensione trovata</h1></div>}
             </section>
             <section className="mb-3">
-                <FormReviews id={id} getMovie={getMovie}/>
+                <FormReviews id={id} getMovie={getMovie} />
             </section>
-            <button className="btn btn-danger" onClick={()=> navigate(-1)}><i className="fa-solid fa-chevron-left"></i> Pagina Precedente</button>
+            <button className="btn btn-danger mb-5" onClick={() => navigate(-1)}><i className="fa-solid fa-chevron-left"></i> Pagina Precedente</button>
         </article>
-        
+
     )
 }
